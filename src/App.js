e367d8fe2ac8e847054fa6f3725b4f1560bdacfb
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-22 10:40:06 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Landing from './components/Landing';
 import Nav from './components/Nav';
 import Highlights from './components/Highlights';
 import Featured from './components/Featured';
+import Discounted from './components/Discounted';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Landing />
       <Highlights />
       <Featured />
+      <Discounted />
     </div>
   );
 }
