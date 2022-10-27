2892a51895fd57c1af7f5b95112bc29a2a81fc89
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-26 20:23:42 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
-import Landing from './components/Landing';
 import Nav from './components/Nav';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Books from './pages/Books';
+import { books } from "./data";
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" render={() => <Books books={books} />} />
       <Footer />
     </div>
+    </Router>
   );
 }
 
