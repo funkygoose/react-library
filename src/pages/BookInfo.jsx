9a981bc56c3113441146ca7f09291a614e42dfeb
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-10-26 21:20:22 -0700

    initial commit

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "../components/ui/Rating";
 
 const BookInfo = ({ books }) => {
   return (
@@ -7,13 +10,30 @@ const BookInfo = ({ books }) => {
         <div className="books__container">
           <div className="row">
             <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/books" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img src="" alt="" className="book__selected--img" />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">Crack the</h2>
+                <Rating rating="4.5"/>
+                <div className="book__selected--price">
 
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </main>
     </div>
   );
-}
+};
 
 export default BookInfo;
