58ca20772179908bdd3a326a114ac6f49fa434ba
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-11-02 09:51:48 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Books from './pages/Books';
 import { books } from "./data";
 import BookInfo from './pages/BookInfo';
+import Cart from './pages/Cart';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/" exact component={Home} />
       <Route path="/books" exact render={() => <Books books={books} />} />
       <Route path="/books/:id" render={() => <BookInfo books={books} />} />
+      <Route path="/cart"  render={() => <Cart books={books} />} />
       <Footer />
     </div>
     </Router>
