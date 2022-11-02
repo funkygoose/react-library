a2e7dc1c62494c232b2c9f8e01f3b2e8171f5cb6
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-11-02 10:37:48 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -13,24 +13,12 @@ function App() {
   const [cart, setCart] = useState([]);
 
   function addToCart(book) {
-    const dupeItem = cart.find((item) => +item.id === +book.id);
-    if (dupeItem) {
-      setCart(
-        cart.map((item) => {
-          if (item.id === dupeItem.id) {
-            return {
-              ...item,
-              quantity: item.quantity + 1,
-            };
-          } else {
-            return item;
-          }
-        })
-      );
-    } else {
-      setCart([...cart, { ...book, quantity: 1 }]);
-    }
+   setCart([book])
   }
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
 
   useEffect(() => {
     console.log(cart);
