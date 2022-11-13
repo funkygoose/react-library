579319790977b964599dfdcb60953afd7700a86e
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2022-11-12 23:19:46 -0800

    initial commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import EmptyCart from "../assets/empty_cart.svg";
 
 const Cart = ({ cart, changeQuantity, removeItem }) => {
   const total = () => {
     let price = 0;
     cart.forEach((item) => {
-      price += +(
-        (item.salePrice || item.originalPrice) * item.quantity
-      );
+      price += +((item.salePrice || item.originalPrice) * item.quantity);
     });
     return price;
   };
@@ -42,7 +42,12 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
                           <span className="cart__book--price">
                             ${(book.salePrice || book.originalPrice).toFixed(2)}
                           </span>
-                          <button className="cart__book--remove" onClick={() => removeItem(book)}>Remove</button>
+                          <button
+                            className="cart__book--remove"
+                            onClick={() => removeItem(book)}
+                          >
+                            Remove
+                          </button>
                         </div>
                       </div>
                       <div className="cart__quantity">
@@ -67,6 +72,13 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
                   );
                 })}
               </div>
+              <div className="cart__empty">
+                <img src={EmptyCart} alt="" className="cart__empty--img" />
+                <h2>You don't have any books in your cart!</h2>
+                <Link to="/books">
+                  <button className="btn">Browse Books</button>
+                </Link>
+              </div>
             </div>
             <div className="total">
               <div className="total__item total__sub--total">
@@ -79,7 +91,7 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
               </div>
               <div className="total__item total__price">
                 <span>Total</span>
-                <span>${(total()).toFixed(2)}</span>
+                <span>${total().toFixed(2)}</span>
               </div>
               <button
                 className="btn btn__checkout no-cursor"
